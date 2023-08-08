a7468bf71c56edde740241bf9e28dc45ab0fd11e
Samuel Henrique <69220234+samhlopes@users.noreply.github.com>
2023-08-08 09:14:39 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyDnJ0PNH8_iN2pUortcYzye_HCk_bJeBVQ",
+  authDomain: "votacao-politicamente-equipada.firebaseapp.com",
+  databaseURL: "https://votacao-politicamente-equipada-default-rtdb.firebaseio.com",
+  projectId: "votacao-politicamente-equipada",
+  storageBucket: "votacao-politicamente-equipada.appspot.com",
+  messagingSenderId: "684052381385",
+  appId: "1:684052381385:web:e06877e1f511ba5d755718"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
